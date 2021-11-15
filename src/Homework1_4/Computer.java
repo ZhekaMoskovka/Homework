0eca7bffa33300ecea52bfda2830d4e99666f218
Zheka Skipper <zheniachok@gmail.com>
2021-11-15 22:17:57 +0200

    added fourth subtask

---
+++ src/Homework1_4/Computer.java
@@ -0,0 +1,12 @@
+package Homework1_4;
+
+public class Computer {
+    public int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+}
---
+++ src/Homework1_4/Homework1_4.java
@@ -0,0 +1,17 @@
+package Homework1_4;
+
+public class Homework1_4 {
+    public static void main(String[] args) {
+        Computer computers [] = new Computer[5];
+
+        for (int i = 0; i < 5; i++){
+           Computer computer = new Computer();
+           computer.setId(i);
+           computers[i] = computer;
+        }
+        for (int i = 0; i < 5; i++){
+            System.out.println(computers[i].getId());
+        }
+    }
+}
+
