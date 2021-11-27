b6f13a26fa6f68dea87401b94c95c1cac9940dce
Zheka Skipper <zheniachok@gmail.com>
2021-11-27 18:22:09 +0200

    fixed mistakes

--- src/Homework4_3/Player.java
+++ src/Homework4_3/Player.java
@@ -1,6 +1,7 @@
 package Homework4_3;
 
 public class Player implements Playable, Recodable {
+    public String previousCommand = "null";
     @Override
     public void play() {
         System.out.println("Playing was started");
@@ -19,5 +20,20 @@ public class Player implements Playable, Recodable {
     @Override
     public void stop() {
         System.out.println("Stopped");
+    }
+
+    public void choice (String command) {
+        if (command.equals("record") && !previousCommand.equals("record") && !previousCommand.equals("play")) {
+            record();
+        } else if (command.equals("play") && !previousCommand.equals("record") && !previousCommand.equals("play")) {
+            play();
+        } else if (command.equals("pause")) {
+            pause();
+        } else if (command.equals("stop")) {
+            stop();
+        } else {
+            System.out.println("error");
+        }
+        previousCommand = command;
     }
 }
