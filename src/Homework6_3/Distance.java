78de4718fa1bd5c3fc6fe2e0b9be0ce85f658b5b
Zheka Skipper <zheniachok@gmail.com>
2021-11-30 17:46:31 +0200

    Homework6

--- src/Homework6_3/Distance.java
+++ src/Homework6_3/Distance.java
@@ -2,6 +2,7 @@ package Homework6_3;
 
 public class Distance {
     double distance;
+
     public void print() {
     }
 
@@ -9,10 +10,12 @@ public class Distance {
         public double convertMetersToKilometers(double meters) {
             return meters / 1000;
         }
+
         public double convertKilometersToMiles(double kilometers) {
             return kilometers / 1.61;
         }
-        public double convertCentimetersToInch(double centimeters){
+
+        public double convertCentimetersToInch(double centimeters) {
             return centimeters / 2.54;
         }
     }
