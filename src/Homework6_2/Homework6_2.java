e398b71d1e22da642e210878363b61b8fdb2b603
Zheka Skipper <zheniachok@gmail.com>
2021-11-30 17:45:44 +0200

    Homework6

---
+++ src/Homework6_2/Homework6_2.java
@@ -0,0 +1,8 @@
+package Homework6_2;
+
+public class Homework6_2 {
+    public static void main(String[] args) {
+        Vehicle.Wheel wheel = new Vehicle().new Wheel();
+        Vehicle.Door door = new Vehicle().new Door();
+    }
+}
---
+++ src/Homework6_2/Vehicle.java
@@ -0,0 +1,14 @@
+package Homework6_2;
+
+public class Vehicle {
+    public void print() {
+    }
+    public class Wheel {
+        public void print() {
+        }
+    }
+    public class Door {
+        public void print() {
+        }
+    }
+}
