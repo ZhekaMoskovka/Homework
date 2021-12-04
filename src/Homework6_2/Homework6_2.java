fd14ca787d39541aa63a0c14fe7e19f193846064
Zheka Skipper <zheniachok@gmail.com>
2021-12-04 13:52:29 +0200

    fixed mistakes

--- src/Homework6_2/Homework6_2.java
+++ src/Homework6_2/Homework6_2.java
@@ -2,7 +2,7 @@ package Homework6_2;
 
 public class Homework6_2 {
     public static void main(String[] args) {
-        Vehicle.Wheel wheel = new Vehicle().new Wheel();
-        Vehicle.Door door = new Vehicle().new Door();
+        Vehicle.Wheel wheel = new Vehicle("audi").new Wheel("левосторонний");
+        Vehicle.Door door = new Vehicle("nissan").new Door(2);
     }
 }
