4945fa67594de57acca185a1f091f7418872e44b
Zheka Skipper <zheniachok@gmail.com>
2022-02-11 15:43:29 +0200

    initialization in other class

--- src/main/java/Homework20_5/Homework20_5.java
+++ src/main/java/Homework20_5/Homework20_5.java
@@ -3,32 +3,21 @@ package Homework20_5;
 import java.sql.*;
 
 public class Homework20_5 {
-    public static void Initialization() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("Driver loaded success");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public static final String URL = "jdbc:mysql://localhost:3306/MyJoinsDB";
-    public static final String LOGIN = "root";
-    public static final String PWD = "Gentelmen098";
+
+
     public static void main(String[] args) {
-        Initialization();
-        Connection connection;
         PreparedStatement contactData;
         PreparedStatement birthdayDateIfNotMarried;
         PreparedStatement birthdayDateIfManager;
 
         try {
-            connection = DriverManager.getConnection(URL, LOGIN, PWD);
-            contactData = connection.prepareStatement("select id, telephone_number, place_of_residance from employees\n" +
+            SQLInitialization sqlInitialization = new SQLInitialization();
+            contactData = sqlInitialization.connection.prepareStatement("select id, telephone_number, place_of_residance from employees\n" +
                     " inner join personal_information on id = employee_id");
-            birthdayDateIfNotMarried = connection.prepareStatement("select id, birthday, telephone_number from employees\n" +
+            birthdayDateIfNotMarried = sqlInitialization.connection.prepareStatement("select id, birthday, telephone_number from employees\n" +
                     "inner join personal_information on id = employee_id where is_married = false");
-            birthdayDateIfManager = connection.prepareStatement("select id, birthday, telephone_number from employees\n" +
+            birthdayDateIfManager = sqlInitialization.connection.prepareStatement("select id, birthday, telephone_number from employees\n" +
                     "inner join positions on id = positions.employee_id\n" +
                     "inner join personal_information on id = personal_information.employee_id where positions.employee_position = 'Менеджер';");
             ResultSet resultSet = contactData.executeQuery();
